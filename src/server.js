0897f0e76ad26938ff8ee973ef31ab6bcfe2cb55
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-01 16:55:17 +0900

    [241201] feat: 홈(/) 경로 외 다른 경로 접근 시 홈 경로로 리다이렉션

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ app.use("/public", express.static(__dirname + "/public"));
 // /views/home.pug 파일을 렌더링 하도록 라우팅 설정
 app.get("/", (req, res) => res.render("home"));
 
+// 사용자가 다른 경로로 접근 시 "/" 경로로 리다리엑션
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
 // 3000번 포트로 서버 실행
