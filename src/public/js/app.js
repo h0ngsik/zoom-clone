e649e56e534ddeea3447ddc1433e104eb11c1620
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-08 11:45:23 +0900

    [241208] WebSocket을 통해, 실시간 채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,42 @@
-alert("안녕");
+// 프론트엔드에서 socket은 서버와의 연결을 의미
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nickname");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server !");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("new message:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("disconnected from Server !");
+});
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+};
+
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -19,8 +19,31 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 // ws, http를 하나의 서버에서 작동할 수 있다. (꼭 함께 사용할 필요는 없음.)
 const wss = new WebSocket.Server({ server });
-
 // app.listen(3000, handleListen)
+
+const sockets = [];
+
+// 서버에서 소켓은 연결된 브라우저를 의미한다.
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser !");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(
+            `${socket.nickname}: ${message.payload.toString("utf8")}`
+          )
+        );
+      case "nickname":
+        console.log(message.payload);
+        socket["nickname"] = message.payload;
+    }
+  });
+});
 
 // http 서버 위에 ws 서비스도 함께 제공.
 server.listen(3000, handleListen);
